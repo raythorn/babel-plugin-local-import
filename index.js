077e3d26842974ca7a4dcb9da88baf7e6a95df5b
leihj <leihj@ljlx.com>
2019-06-28 12:29:08 +0800

    support expand import path, and import component on demand

--- index.js
+++ index.js
@@ -1,31 +1,45 @@
 const filepath = require('path');
 
+function _camel2dash(_name) {
+    const name = _name[0].toLowerCase() + _name.substr(1);
+    return name.replace(/([A-Z])/g, ($1) => `-${$1.toLowerCase()}`);
+}
+
 module.exports = function LocalImport({ types: t }) {
     return {
         visitor: {
             ImportDeclaration: {
                 enter(path, state) {
                     const source = path.get('source');
-                    let { opts: { libraryName, libraryDirectory } } = state;
+                    let { opts: { libraryName, libraryDirectory, subdirectory, camel2dash, expand } } = state;
                     const srcdir = filepath.parse(state.file.opts.filename).dir;
                     if (source.node.value !== libraryName) {
                         return;
                     }
 
                     libraryDirectory = libraryDirectory || '';
+                    subdirectory = subdirectory || '';
 
                     let acturalImportPath = filepath.join(filepath.relative(srcdir, libraryDirectory), libraryName);
                     acturalImportPath = acturalImportPath.replace(/\\/g, '/');
                     if (acturalImportPath[0] !== '.') {
                         acturalImportPath = './' + acturalImportPath;
                     }
-                    source.replaceWith(t.stringLiteral(acturalImportPath));
-                    // TODO:
-                    // const newImports = path.node.specifiers.map( item => {
-                    //     return t.importDeclaration([t.importDefaultSpecifier(item.local)], t.stringLiteral(`${alias}/${item.local.name}`))
-                    // });
-                    // path.replaceWithMultiple(newImports);
-                    // should handle import { A as B } from 'C'
+
+                    if (!expand) {
+                        source.replaceWith(t.stringLiteral(acturalImportPath));
+                        return;
+                    }
+
+                    if (subdirectory !== '') acturalImportPath = acturalImportPath + '/' + subdirectory;
+                    const newImports = path.node.specifiers.map( item => {
+                        let name = item.imported.name;
+                        if (camel2dash) {
+                            name = _camel2dash(name);
+                        }
+                        return t.importDeclaration([t.importDefaultSpecifier(item.local)], t.stringLiteral(`${acturalImportPath}/${name}`))
+                    });
+                    path.replaceWithMultiple(newImports);
                 }
             }
         }
